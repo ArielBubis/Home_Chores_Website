9d7929b84b1972c403086c25e1034eae94a9a54a
Narkisii <narkiser99@gmail.com>
2024-07-24 16:43:20 +0300

    added button in install.php page to lead the user straight to login page after installing the database

--- API/install.php
+++ API/install.php
@@ -1,78 +1,74 @@
 <?php
-    mysqli_report(MYSQLI_REPORT_OFF);
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
+mysqli_report(MYSQLI_REPORT_OFF);
+$servername = "localhost";
+$username = "root";
+$password = "";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    
-    // Create database
-    $dbName = "hw2_205735749_208019729";
-    $sql = "CREATE DATABASE IF NOT EXISTS $dbName";
-    if ($conn->query($sql)) {
-        echo "Database created successfully <br>";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-        // Select the newly created database
-        $conn->select_db($dbName);
-        
-        require_once('create_tables.php');
-        create_users_table($conn);
-        create_chores_list_table($conn);
-        create_chrores_table($conn);
-        create_responsible_for_list_table($conn);
+// Create database
+$dbName = "hw2_205735749_208019729";
+$sql = "CREATE DATABASE IF NOT EXISTS $dbName";
+if ($conn->query($sql)) {
+    echo "Database created successfully <br>";
 
-    } else{
-        echo"Error creating data base:".$conn->error . "<br>";
-        die();
-    }
-    
-    // ---- Insert values to tables ----
+    // Select the newly created database
+    $conn->select_db($dbName);
 
-    // Add test yser record 
-    $password = 'test123';
-    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-    
-    $sql = "INSERT INTO Users(email, first_name, last_name, avatar_color, password) VALUES('test@test.com', 'John', 'Doe','43a047', '$passwordHash')"; 
-    
-    if($conn->query($sql)) {
-        echo "Test user added successfully<br>";
-    }
-    else {
-        echo "Error: " . $sql . " " . $conn->error . "<br>";
-    }
-    
+    require_once('create_tables.php');
+    create_users_table($conn);
+    create_chores_list_table($conn);
+    create_chrores_table($conn);
+    create_responsible_for_list_table($conn);
+} else {
+    echo "Error creating data base:" . $conn->error . "<br>";
+    die();
+}
 
-    // Add chores lists records
-    $sql = "INSERT INTO Chores_List(list_title, due_date, status)
+// ---- Insert values to tables ----
+
+// Add test yser record 
+$password = 'test123';
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO Users(email, first_name, last_name, avatar_color, password) VALUES('test@test.com', 'John', 'Doe','43a047', '$passwordHash')";
+
+if ($conn->query($sql)) {
+    echo "Test user added successfully<br>";
+} else {
+    echo "Error: " . $sql . " " . $conn->error . "<br>";
+}
+
+
+// Add chores lists records
+$sql = "INSERT INTO Chores_List(list_title, due_date, status)
     VALUES
         ('House chores', '2024-06-07', 0),
         ('Shopping' ,'2024-06-08', 0)";
-    if($conn->query($sql)) {
-        echo "2 Chores lists added successfully<br>";
-    }
-    else {
-        echo "Error: " . $sql . " " . $conn->error . "<br>";
-    }
+if ($conn->query($sql)) {
+    echo "2 Chores lists added successfully<br>";
+} else {
+    echo "Error: " . $sql . " " . $conn->error . "<br>";
+}
 
-    // Add reponsible users to chores list
-    $sql = "INSERT INTO Responsible_For_List(user_id, list_id)
+// Add reponsible users to chores list
+$sql = "INSERT INTO Responsible_For_List(user_id, list_id)
     VALUES
         (1, 1),
         (1, 2)";
-    if($conn->query($sql)) {
-        echo "Reponsible users assigned successfully<br>";
-    }
-    else {
-        echo "Error: " . $sql . " " . $conn->error . "<br>";
-    }
+if ($conn->query($sql)) {
+    echo "Reponsible users assigned successfully<br>";
+} else {
+    echo "Error: " . $sql . " " . $conn->error . "<br>";
+}
 
-    // Add chores to chores lists 
-    $sql = "INSERT INTO Chores(list_id, chore_title, date_added, user_id, finished)
+// Add chores to chores lists 
+$sql = "INSERT INTO Chores(list_id, chore_title, date_added, user_id, finished)
     VALUES
         (1, 'Clean the house', '2024-06-07', 1, 0),
         (1, 'Organize the closet' ,'2024-06-07', 1, 0),
@@ -82,18 +78,37 @@
         (2, 'Buy wall paint' ,'2024-06-09', 1, 0),
         (2, 'Buy paintbrush', '2024-06-09', 1, 0)";
 
-    if($conn->query($sql)) {
-        echo "Chores added successefully to chores lists";
-    }
-    else {
-        echo "Error: " . $sql . " " . $conn->error . "<br>";
-    }
+if ($conn->query($sql)) {
+    echo "Chores added successefully to chores lists";
+} else {
+    echo "Error: " . $sql . " " . $conn->error . "<br>";
+}
 
-    // $stmt = $conn->prepare("INSERT INTO Users(
-    //     email, first_name, last_name, password) VALUES(?,?,?,?)");
-    // $stmt->bind_param("ssss", $email,$first_name, $last_name, $password);
+// $stmt = $conn->prepare("INSERT INTO Users(
+//     email, first_name, last_name, password) VALUES(?,?,?,?)");
+// $stmt->bind_param("ssss", $email,$first_name, $last_name, $password);
 
-    $conn->close();
-    
+$conn->close();
+
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create database</title>
+
+</head>
+
+<body>
+    <div class="container all_style">
+        <br>
+        <a href="../log_in.php">
+            <button type="button">Back to site</button>
+        </a>
+    </div>
+</body>
+
+</html>
